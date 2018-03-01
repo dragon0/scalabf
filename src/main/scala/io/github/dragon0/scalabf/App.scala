98703811fe8b1d52d58174e552eea6d1d651cc68
Sean Raven <sravendragon0@hotmail.com>
2018-02-28 17:41:35 -0800

    added CLI

--- src/main/scala/io/github/dragon0/scalabf/App.scala
+++ src/main/scala/io/github/dragon0/scalabf/App.scala
@@ -1,8 +1,25 @@
 package io.github.dragon0.scalabf;
+import io.github.dragon0.scalabf.parser._
+import java.util.Scanner
 
 object App {
     def main(args: Array[String]): Unit = {
-        println("Hello")
+        val in = new Scanner(System.in)
+        print(": ")
+        while(in.hasNext){
+            val word = in.next
+            val tokens = Parser.tokenize(word)
+            tokens match {
+                case Left(ut) => {
+                    printf("Unrecognized character %c at position %d\n", ut.c, ut.location)
+                }
+                case Right(seq) => {
+                    println(seq)
+                }
+            }
+            print(": ")
+        }
+        println()
     }
 }
 
